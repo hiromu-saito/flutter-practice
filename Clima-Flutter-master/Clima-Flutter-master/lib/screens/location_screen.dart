6eea86d26e90eca328241ffab0ad1d40cac38cb8
hiromu_saito <hiromu.saito@bigtreetc.com>
2021-09-25 20:44:39 +0900

    make clima app

---
+++ Clima-Flutter-master/Clima-Flutter-master/lib/screens/location_screen.dart
@@ -0,0 +1,124 @@
+import 'package:clima/screens/city_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:clima/utilities/constants.dart';
+import 'package:clima/services/weather.dart';
+
+class LocationScreen extends StatefulWidget {
+  LocationScreen({this.locationWeather});
+  final locationWeather;
+
+  @override
+  _LocationScreenState createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  WeatherModel weatherModel = WeatherModel();
+  int temp;
+  String weatherIcon = '';
+  String cityName;
+  String weatherMessage = '';
+
+  @override
+  void initState() {
+    super.initState();
+    updateUi(widget.locationWeather);
+  }
+
+  void updateUi(dynamic weatherData) {
+    setState(() {
+      if (weatherData == null) {
+        cityName = '';
+        weatherIcon = '';
+        temp = 0;
+        weatherMessage = 'Unnable to get weather Data';
+        return;
+      }
+
+      temp = weatherData['main']['temp'].toInt();
+      var condition = weatherData['weather'][0]['id'];
+      weatherIcon = WeatherModel().getWeatherIcon(condition);
+      cityName = weatherData['name'];
+      weatherMessage = WeatherModel().getMessage(temp);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/location_background.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.white.withOpacity(0.8), BlendMode.dstATop),
+          ),
+        ),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  FlatButton(
+                    onPressed: () async {
+                      var weatherData = await weatherModel.getWeatherData();
+                      updateUi(weatherData);
+                    },
+                    child: Icon(
+                      Icons.near_me,
+                      size: 50.0,
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: () async {
+                      var cityName = await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => CityScreen()));
+                      if (cityName != null) {
+                        var weatherData = await weatherModel
+                            .getWeatherDataByCityName(cityName);
+                        updateUi(weatherData);
+                      }
+                    },
+                    child: Icon(
+                      Icons.location_city,
+                      size: 50.0,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 15.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      '$temp°',
+                      style: kTempTextStyle,
+                    ),
+                    Text(
+                      weatherIcon,
+                      style: kConditionTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 15.0),
+                child: Text(
+                  '$weatherMessage in $cityName',
+                  textAlign: TextAlign.right,
+                  style: kMessageTextStyle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ Clima-Flutter-master/Clima-Flutter-master/lib/services/weather.dart
@@ -0,0 +1,56 @@
+import 'package:clima/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = '903b10171e0a8d16383f91ff6b43ce5c';
+
+class WeatherModel {
+  Future<dynamic> getWeatherDataByCityName(String cityName) async {
+    NetworkHelper networkHelper =
+        NetworkHelper('$apiUrl?units=metric&q=$cityName&appid=$apiKey');
+    var weahterData = await networkHelper.getData();
+    return weahterData;
+  }
+
+  Future<dynamic> getWeatherData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$apiUrl?units=metric&lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey');
+    var weahterData = await networkHelper.getData();
+    return weahterData;
+  }
+
+  String getWeatherIcon(int condition) {
+    if (condition < 300) {
+      return '🌩';
+    } else if (condition < 400) {
+      return '🌧';
+    } else if (condition < 600) {
+      return '☔️';
+    } else if (condition < 700) {
+      return '☃️';
+    } else if (condition < 800) {
+      return '🌫';
+    } else if (condition == 800) {
+      return '☀️';
+    } else if (condition <= 804) {
+      return '☁️';
+    } else {
+      return '🤷‍';
+    }
+  }
+
+  String getMessage(int temp) {
+    if (temp > 25) {
+      return 'It\'s 🍦 time';
+    } else if (temp > 20) {
+      return 'Time for shorts and 👕';
+    } else if (temp < 10) {
+      return 'You\'ll need 🧣 and 🧤';
+    } else {
+      return 'Bring a 🧥 just in case';
+    }
+  }
+}
